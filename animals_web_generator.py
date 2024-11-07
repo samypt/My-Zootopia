debf07735f0aa36efe1636dbd45e53badc15a330
samypt <vitaliiptichkin@gmail.com>
2024-11-07 10:05:37 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,20 +1,32 @@
 import json
 
+NEW_HTML_TEMPLATE = "animals.html"
+
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
+def load_template(file_path):
+    with open(file_path, "r") as handle:
+        return handle.read()
 
-def print_data(data):
+def save_template(file_path, content):
+    with open(file_path, "w") as handle:
+        handle.write(content)
+
+
+def access_data(data):
+    output = ""
     for animal in data:
-        print('Name: ', animal['name'])
-        print('Diet: ',animal['characteristics']['diet'])
-        print('Location: ',animal['locations'][0])
+        output += f"'Name: '{animal['name']}\n"
+        output += f"'Diet: '{animal['characteristics']['diet']}\n"
+        output += f"'Location: '{animal['locations'][0]}\n"
         if 'type' in animal['characteristics']:
-            print('Type: ',animal['characteristics']['type'])
-        print('\n')
+            output += f"'Type: '{animal['characteristics']['type']}\n"
+        output += "\n"
+    return output
 
 
-
-print_data(load_data("animals_data.json"))
+new_data = load_template("animals_template.html").replace("__REPLACE_ANIMALS_INFO__",access_data(load_data("animals_data.json")))
+save_template(NEW_HTML_TEMPLATE, new_data)
